5a878c003ee9363b3030a566fec37212a7c6c0a7
piter26133 <p.paszek@o2.pl>
2020-06-16 15:21:31 +0200

    aktualizacja

--- public/js.d818e0ef.js
+++ public/js.d818e0ef.js
@@ -118,7 +118,7 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"src/js/index.js":[function(require,module,exports) {
-var listing = document.querySelector('.posts__listing');
+var listing = document.querySelector('.post__listing');
 var template = document.getElementById('post-template');
 var templateHtml = template.innerHTML;
 var listHtml = "";
@@ -180,7 +180,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56136" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63167" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-var listing = document.querySelector('.posts__listing');
+var listing = document.querySelector('.post__listing');
 var template = document.getElementById('post-template');
 var templateHtml = template.innerHTML;
 var listHtml = "";
@@ -9,7 +9,7 @@ function processArticle(art) {
     .replace(/{{alt}}/g, art.alt)
     .replace(/{{name}}/g, art.name)
     .replace(/{{price}}/g, art.price)
-    .replace(/{{sizes}}/g, art?.sizes?.map(function(item) {
+    .replace(/{{sizes}}/g, art ? .sizes ? .map(function (item) {
       return `<label>
               <input type="radio" name="size" value="${item}" />
                 ${item}
@@ -19,22 +19,22 @@ function processArticle(art) {
 }
 
 
-function loadJSON(url, callback) {   
+function loadJSON(url, callback) {
 
-    var xobj = new XMLHttpRequest();
-        xobj.overrideMimeType("/products.json");
-    xobj.open('GET', url, true);
-    xobj.onreadystatechange = function () {
-          if (xobj.readyState == 4 && xobj.status == "200") {
-            callback(xobj.responseText);
-          }
-    };
-    xobj.send(null);  
- }
+  var xobj = new XMLHttpRequest();
+  xobj.overrideMimeType("/products.json");
+  xobj.open('GET', url, true);
+  xobj.onreadystatechange = function () {
+    if (xobj.readyState == 4 && xobj.status == "200") {
+      callback(xobj.responseText);
+    }
+  };
+  xobj.send(null);
+}
 
-loadJSON('https://run.mocky.io/v3/e67f8676-6ef9-4991-b89d-7eb601ed3a38', function(data) {
-  var productsObject = JSON.parse(data); 
-  productsObject.products.filter(function(item){
+loadJSON('https://run.mocky.io/v3/e67f8676-6ef9-4991-b89d-7eb601ed3a38', function (data) {
+  var productsObject = JSON.parse(data);
+  productsObject.products.filter(function (item) {
     return item.category.indexOf("ALL") >= 0;
   }).map(processArticle);
 })
